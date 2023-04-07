f360c4c96275fc847f1547bd2c31ec0c881f7f5d
Leo Ramirez Argume <pcsilram@upc.edu.pe>
2023-04-07 16:45:42 +0200

    Proyecto Basico onewebs

---
+++ src/main/java/pe/edu/upc/onewebs/entity/Apelacion.java
@@ -0,0 +1,26 @@
+package pe.edu.upc.onewebs.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "apelacion")
+@Getter
+@Setter
+public class Apelacion {
+	@Id
+	private Long id;
+
+	@Column(name = "apelador2", length = 50, nullable = false)
+	private Long apelador2;
+	
+	@NotBlank(message = "Es obligatorio el apelador")
+	@Column(name = "apelador", length = 50, nullable = false)	
+	private String apelador;
+}
